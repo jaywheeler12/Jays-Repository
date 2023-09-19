9dc9b07adf62a3630db17f77fc3683f4fb29d98d
Jay Wheeler <jay.wheeler@mrjaffesclass.com>
2023-09-19 11:31:24 -0700

    While-loops and Do-While-Loops complete

--- Jay-Project/FirstCode1.java
+++ Jay-Project/FirstCode1.java
@@ -9,44 +9,23 @@
 public class FirstCode1
 {
 
-    int a = 5;
-    int b = 2;
-    int c = 1;
-    int d = -8;
-    
+    int i = 14;
+    int k = 6;
+    int a = 9;
+    int b = 20;
+
     public void run() {
-    if (a == 5) {
-        System.out.println("SUCCESS");
-    } else {
-        System.out.println("FAILURE");
-        
+        do {
+            System.out.println("TRUE");
+        } while (i < 15);  
+        do {
+            System.out.println("TRUE");
+        } while (k > 5);
+        do {
+            System.out.println("TRUE");
+        } while (a < 10 && b <= 20);
     }
-    if (b == 2) {
-        System.out.println("SUCCESS");
-    } else {
-        System.out.println("FAILURE");
-        
-    }
-    if (c == 2) {
-        System.out.println("FAILURE");
-    } else {
-        System.out.println("SUCCESS");
-        
-    }
-    if (c != 2) {
-        System.out.println("SUCCESS");
-    } else {
-        System.out.println("FAILURE");
-        
-    }
-    if (d == -8 && a == 4) {
-        System.out.println("SUCCESS");
-    } else {
-        System.out.println("FAILURE");
-        
-    }
-}
-    
+
     public static void main(String[] args)
     {        
         FirstCode1 hw = new FirstCode1();
