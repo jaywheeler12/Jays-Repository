9838d8ce80f929578729becc88ed92132d87e0f8
Jay Wheeler <jay.wheeler@mrjaffesclass.com>
2023-10-31 11:01:38 -0700

    FizzBuzz complete

--- Jay-Project/FirstCode1.java
+++ Jay-Project/FirstCode1.java
@@ -11,9 +11,11 @@ public class FirstCode1
     public static void main(String[] args)
     {    
        for (int i = 1; i <= 100; i++) {
-           if ((i%3) == 0) {
+           if ((i/15) * 15 == i){
+               System.out.println("FizzBuzz");
+           }else if ((i/3) * 3 == i){
                System.out.println("Fizz");
-           }else if ((i%5) == 0){
+           }else if ((i/5) * 5 == i) {
                System.out.println("Buzz");
            }else{
                System.out.println(i);
