dc30e7e2fda3be59abb550bad279ffcb0ef11d5c
Jay Wheeler <jay.wheeler@mrjaffesclass.com>
2023-12-11 11:46:09 -0800

    la

--- Jay-Project/DieN.java
+++ Jay-Project/DieN.java
@@ -1,5 +1,19 @@
-public class DieN {
-
-
+public class DieN
+{
+  private int value;
+  public DieN() {
+    this.roll();
+  }
+  public int getValue() {
+    return value;
+  }
+  public void roll() {
+    this.value = (int)(Math.random() * 20) + 1;
+  }
+  public int rollAndGetValue() {
+    roll();
+    return getValue();
+  }
 
 }
+
