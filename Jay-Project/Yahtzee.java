79ffea474373f44905514db11a4ff0f8fc683e5c
Jay Wheeler <jay.wheeler@mrjaffesclass.com>
2023-12-14 11:40:58 -0800

    hello

--- Jay-Project/Yahtzee.java
+++ Jay-Project/Yahtzee.java
@@ -1,22 +1,15 @@
 public class Yahtzee
 {
-  private Die6 die1;
-  private Die6 die2;
+
   
   public Yahtzee() {
-      die1 = new Die6();
-      die2 = new Die6();
+  
   }
   public int getValue() {
-    return die1.getValue() + die2.getValue();
   }
   public void roll() {
-    die1.roll();
-    die2.roll();
   }
   public int rollAndGetValue() {
-    roll();
-    return getValue();
   }
 
 }
