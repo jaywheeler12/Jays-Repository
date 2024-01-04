07213459e9a8feb808718bf9db0b4ca6a4b59a92
Jay Wheeler <jay.wheeler@mrjaffesclass.com>
2024-01-04 11:33:34 -0800

    sss

--- Jay-Project/Yahtzee.java
+++ Jay-Project/Yahtzee.java
@@ -1,33 +1,60 @@
-public class Yahtzee {
-
-    public Die6 die1, die2, die3, die4, die5;
-
-    public Yahtzee() {
-        die1 = new Die6();
-        die2 = new Die6();
-        die3 = new Die6();
-        die4 = new Die6();
-        die5 = new Die6();
+public class Yahtzee
+{
+    Die6[] dice = new Die6[5];
+    public Yahtzee()
+    {
+        for (int i = 0; i < dice.length; i++) {
+            dice[i] = new Die6();
+        }
     }
 
     public void roll() {
-        die1.roll();
-        die2.roll();
-        die3.roll();
-        die4.roll();
-        die5.roll();
+        for (int i = 0; i < dice.length; i++) {
+            dice[i].roll();
+        }
+    }
+
+    public void roll(int dieNumber) {
+        if (dieNumber == 1) {
+            dice[0].roll();
+        } else if (dieNumber == 2) {
+            dice[1].roll();
+        } else if (dieNumber == 3) {
+            dice[2].roll();
+        } else if (dieNumber == 4) {
+            dice[3].roll();
+        } else if (dieNumber == 5) {
+            dice[4].roll();
+        }
     }
 
     public String summarize() {
-        return die1.getValue() + "- 1";
-        return die2.getValue() + "- 2";
-        return die3.getValue() + "- 3";
-        return die4.getValue() + "- 4";
-        return die5.getValue() + "- 5";
+        int ones = 0;
+        int twos = 0;
+        int threes = 0;
+        int fours = 0;
+        int fives = 0;
+        int sixes = 0;
+        for (int i = 0; i < dice.length; i++) {
+            if (dice[i].getValue() == 1) {
+                ones++;
+            } else if (dice[i].getValue() == 2) {
+                twos++;
+            } else if (dice[i].getValue() == 3) {
+                threes++;
+            } else if (dice[i].getValue() == 4) {
+                fours++;
+            } else if (dice[i].getValue() == 5) {
+                fives++;
+            } else if (dice[i].getValue() == 6) {
+                sixes++;
+            }
+        }
+        return "1 - " + ones + ", 2 - " + twos + ", 3 - " + threes + ", 4 - " + fours + ", 5 - " + fives + ", 6 - " + sixes;
     }
 
     public String toString() {
-        return die1.getValue() + " " + die2.getValue() + " " +
-        die3.getValue() + " " + die4.getValue() + " " + die5.getValue();
+        String dieStr = "Dice Values: " + dice[0].getValue() + " " + dice[1].getValue() + " " + dice[2].getValue() + " " + dice[3].getValue() + " " + dice[4].getValue();  
+        return dieStr;
     }
 }
