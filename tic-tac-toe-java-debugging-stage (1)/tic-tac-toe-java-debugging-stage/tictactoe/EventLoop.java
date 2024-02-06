c4336c0eaa711f4616840b031fe2752e1db3dc2e
Jay Wheeler <jay.wheeler@mrjaffesclass.com>
2024-02-06 11:38:10 -0800

    yo

--- tic-tac-toe-java-debugging-stage (1)/tic-tac-toe-java-debugging-stage/tictactoe/EventLoop.java
+++ tic-tac-toe-java-debugging-stage (1)/tic-tac-toe-java-debugging-stage/tictactoe/EventLoop.java
@@ -6,31 +6,37 @@ public class EventLoop {
   State state = new State();
   UI ui = new UI();
   int row, col;
+  public static void main(String[] args) {
+      EventLoop eventLoop = new EventLoop();
+      eventLoop.run();
+  }
 
-  public void eventLoop() {
+  public void run() {
     while (state.getGameState() != Constants.QUIT_PROGRAM) {
       int gameState = state.getGameState();
       if (gameState == Constants.STANDBY) {
         state.setGameState(Constants.GET_X_NAME);
 
       } else if (gameState == Constants.GET_X_NAME) {
-        state.setXName(ui.promptForName(Constants.X));
+        state.setXName(ui.promptForName("X"));
         state.setGameState(Constants.GET_O_NAME);
     
       } else if (gameState == Constants.GET_O_NAME) {
-        state.setYName(ui.promptForName(Constants.O));
+        state.setOName(ui.promptForName("O"));
         state.setGameState(Constants.GET_X_MOVE);
     
       } else if (gameState == Constants.GET_X_MOVE) {
-        row = ui.getMoveRow(state.getWhoseMove());
-        col = ui.getMoveCol(state.getWhoseMove());
+          ui.printBoard(state); 
+        row = ui.getMoveRow(state.getWhoseMove(), state.getXName(), state.getOName());
+        col = ui.getMoveCol(state.getWhoseMove(), state.getXName(), state.getOName());
         if (ui.isLegalMove(state, row, col)) {
           state.setGameState(Constants.MAKE_MOVE);
         }
 
       } else if (gameState == Constants.GET_O_MOVE) {
-        row = ui.getMoveRow(state.getWhoseMove());
-        col = ui.getMoveCol(state.getWhoseMove());
+           ui.printBoard(state); 
+        row = ui.getMoveRow(state.getWhoseMove(), state.getXName(), state.getOName());
+        col = ui.getMoveCol(state.getWhoseMove(), state.getXName(), state.getOName());
         if (ui.isLegalMove(state, row, col)) {
           state.setGameState(Constants.MAKE_MOVE);
         }
@@ -53,10 +59,10 @@ public class EventLoop {
 
       } else if (gameState == Constants.CHECK_IF_TIE) {
         if (state.isTie()) {
-          ui.printTie();
+          ui.printTieGame();
           state.setGameState(Constants.GAME_OVER);
         } else {
-          state.setWhoseMove(state.getWhoseMove() * -1);
+          state.setWhoseMore(state.getWhoseMove() * -1);
           if (state.getWhoseMove() == Constants.X) {
             state.setGameState(Constants.GET_X_MOVE);
           } else {
